60ccfd824315c606106d81c654a264865e4d61ac
mnursey <mitchnursey@gmail.com>
2020-04-14 18:14:55 -0700

    Created Basic Menu Flow

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,20 +6,24 @@ using TMPro;
 public class MenuController : MonoBehaviour
 {
     public GameObject mainMenu;
+    public GameObject gameMenu;
+    public GameObject gameUI;
+
     public TMP_InputField serverIP;
     public ClientController cc;
     public RaceController rc;
 
     public void MainMenuPlay()
     {
-        mainMenu.gameObject.SetActive(false);
-
         if(serverIP.text != "")
         {
             cc.serverIP = serverIP.text;
         }
 
         cc.ConnectToServer();
+
+        DisableMainMenuUI();
+        EnableGameMenuUI();
     }
 
     public void MainMenuOptions()
@@ -30,5 +34,52 @@ public class MenuController : MonoBehaviour
     public void MainMenuQuit()
     {
         Application.Quit();
+    }
+
+    public void EnableGameMenuUI()
+    {
+        gameMenu.gameObject.SetActive(true);
+    }
+
+    public void DisableGameMenuUI()
+    {
+        gameMenu.gameObject.SetActive(false);
+    }
+
+    public void EnableMainMenuUI()
+    {
+        mainMenu.gameObject.SetActive(true);
+    }
+
+    public void DisableMainMenuUI()
+    {
+        mainMenu.gameObject.SetActive(false);
+    }
+
+    public void EnableGameUI()
+    {
+        gameUI.gameObject.SetActive(true);
+    }
+
+    public void DisableGameUI()
+    {
+        gameUI.gameObject.SetActive(false);
+    }
+
+    public void BackMenu()
+    {
+
+    }
+
+    public void LeaveRace()
+    {
+
+    }
+
+    public void SpawnCar()
+    {
+        DisableGameMenuUI();
+        EnableGameUI();
+        rc.RequestToSpawnCar();
     }
 }
