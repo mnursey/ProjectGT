96ee032b37bacb5a1b2c4050f51ba94729cdea9b
mnursey <mitchnursey@gmail.com>
2020-04-24 03:09:27 -0700

    Audio Attempt 2

--- Assets/Scripts/CarSoundManager.cs
+++ Assets/Scripts/CarSoundManager.cs
@@ -10,6 +10,9 @@ public class CarSoundManager : MonoBehaviour
     public float exhaustPitchIdle = 0.1f;
     public float exhaustPitchRedline = 1.5f;
 
+    public float exhaustVolumeIdle = 0.1f;
+    public float exhaustVolumeRedline = 1.5f;
+
     [Range(0.0f, 1.0f)]
     public float rpmPercent;
 
@@ -17,6 +20,7 @@ public class CarSoundManager : MonoBehaviour
     {
         exhaustSource.loop = true;
         exhaustSource.Play();
+        exhaustSource.velocityUpdateMode = AudioVelocityUpdateMode.Dynamic;
     }
 
     // Update is called once per frame
@@ -28,6 +32,7 @@ public class CarSoundManager : MonoBehaviour
     void calculateExhuast(float rpmPercent)
     {
         exhaustSource.pitch = Mathf.Lerp(exhaustPitchIdle, exhaustPitchRedline, rpmPercent);
+        exhaustSource.volume = Mathf.Lerp(exhaustVolumeIdle, exhaustVolumeRedline, rpmPercent);
     }
 
     public void DisableSounds()
