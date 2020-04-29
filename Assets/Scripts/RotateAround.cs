cd141a76a16640c68cc50d2227750dbaa6027873
mnursey <mitchnursey@gmail.com>
2020-04-29 01:08:35 -0700

    Added clouds

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public Transform centreObject;
+    public float speed = 1.0f;
+    public float distance = 60.0f;
+
+    void Awake()
+    {
+        transform.position = new Vector3(centreObject.position.x + distance, centreObject.position.y, centreObject.position.z);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(centreObject.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
