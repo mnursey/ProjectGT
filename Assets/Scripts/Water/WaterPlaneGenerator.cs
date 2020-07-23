c86d58a13728af62466e93864c494dc2f64b6fa8
mnursey <mitchnursey@gmail.com>
2020-07-23 16:13:32 -0700

    Added basic water

---
+++ Assets/Scripts/Water/WaterPlaneGenerator.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPlaneGenerator : MonoBehaviour
+{
+    public float size = 1;
+    public int gridSize = 16;
+
+    public float power = 3;
+    public float scale = 1;
+    public float timeScale = 1;
+
+    private float offsetX;
+    private float offsetY;
+
+    private MeshFilter filter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filter = GetComponent<MeshFilter>();
+        filter.mesh = GenerateMesh();
+        UpdateNoise();
+    }
+
+    private Mesh GenerateMesh()
+    {
+        Mesh m = new Mesh();
+
+        List<Vector3> verticies = new List<Vector3>();
+        List<Vector3> normals = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+
+        for(int x = 0; x < gridSize + 1; x++)
+        {
+            for(int y = 0; y < gridSize + 1; y++)
+            {
+                verticies.Add(new Vector3(-size * 0.5f + size * (x / ((float)gridSize)), 0, -size * 0.5f + size * (y / ((float)gridSize))));
+                normals.Add(Vector3.up);
+                uvs.Add(new Vector2(x / (float)gridSize, y / (float)gridSize));
+            }
+        }
+
+        List<int> triangles = new List<int>();
+        int vertCount = gridSize + 1;
+
+        for(int i = 0; i < vertCount * vertCount - vertCount; i++)
+        {
+            if((i + 1) % vertCount == 0)
+            {
+                continue;
+            }
+
+            triangles.AddRange(new List<int>() {
+                i + 1 + vertCount, i + vertCount, i,
+                i, i + 1, i + vertCount + 1
+            });
+        }
+
+        m.SetVertices(verticies);
+        m.SetNormals(normals);
+        m.SetUVs(0, uvs);
+        m.SetTriangles(triangles, 0);
+
+        return m;
+    }
+
+    void Update()
+    {
+        UpdateNoise();
+        offsetX += Time.deltaTime * timeScale;
+        offsetY += Time.deltaTime * timeScale;
+    }
+
+    void UpdateNoise()
+    {
+        Vector3[] verticies = filter.mesh.vertices;
+
+        for(int i = 0; i < verticies.Length; i++)
+        {
+            verticies[i].y = CalculateHeight(verticies[i].x, verticies[i].z) * power;
+        }
+
+        filter.mesh.vertices = verticies;
+    }
+
+    float CalculateHeight(float x, float y)
+    {
+        float xCord = x * scale + offsetX;
+        float yCord = y * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCord, yCord);
+    }
+}
