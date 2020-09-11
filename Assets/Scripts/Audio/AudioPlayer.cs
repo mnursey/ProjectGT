ced86048963fff6a33054b4079424c0ad8b776d6
mnursey <mitchnursey@gmail.com>
2020-09-11 01:51:06 -0700

    Added sound on lap complete

---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioSource sound;
+
+    public void Awake()
+    {
+        if (sound == null)
+        {
+            sound = GetComponent<AudioSource>();
+        }
+    }
+
+    public void PlaySound()
+    {
+        sound.Play();
+    }
+}
